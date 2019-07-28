7757be33c6efa352422c048d1f7a494ebe7f530c
Yujeen Han <jeenstar11@gmail.com>
2019-07-29 08:08:35 +0900

    mml write 업데이트
    1. 파일 추가
    mml_write.css
    mml_add.png
    yj_js.js(모달 관련 js -> 미완성)
    mmlWriteModal.jsp(모달 관련 jsp파일 -> 미완성)
    mmlWrite2.jsp
    2. 파일 수정
    MmlController.java(mmlWrite(), mmlWriteAction())
    MmlDAO.java(insertMml())
    MmlService.java(insertMml())
    MmlServiceImpl.java(insertMml())
    MmlMapper.xml(insertMml())
    servlet-context.xml

--- MovieProject/src/main/java/com/spring/mml/MmlController.java
+++ MovieProject/src/main/java/com/spring/mml/MmlController.java
@@ -33,24 +33,38 @@ public class MmlController {
 	public String mmlWrite(HttpSession session, Model model) 
 	{
 		String m_email = (String)session.getAttribute("m_email");
-		System.out.println("=============MmlController.java===================== m_email : " + m_email);
+		//System.out.println("=============MmlController.java===================== m_email : " + m_email);
 		//세션이 없을경우 로그인페이지로 이동....근데 모달인데?
 		if(m_email == null) {
-			System.out.println("=============MyPageController.java===================== m_name == null : " + m_email);
+			//System.out.println("=============MyPageController.java===================== m_name == null : " + m_email);
 			return "redirect:/index.do";
 		}
 		
 		int id = myPageService.getMemberId(m_email);
 		model.addAttribute("id", id);
 		
-		return "mml/mmlWrite";
+		return "mml/mmlWrite2";
 	}
 	
-	@RequestMapping(value="/mmlWriteAction.do", method=RequestMethod.GET)
+	@RequestMapping(value="/mmlWriteAction.do", method=RequestMethod.POST)
 	public String mmlWriteAction(Mml_ContentVO mmlContentVO) 
 	{
 		//mml 작성이 성공적으로 완료되면 작성했던글을 볼 수 있게 연결
-		return "mml/mmlGet";
+		//넘어온 값을 토대로 db에 넣는 작업 진행
+		//System.out.println("=============MmlController.java===================== mmlContentVO.getId() : " + mmlContentVO.getId());
+		//mml_title, mml_content의 앞뒤 공백 제거
+		mmlContentVO.setMml_title(mmlContentVO.getMml_title().trim());
+		
+		try {
+			int result = mmlService.insertMml(mmlContentVO);
+			if(result == 0) {
+				return "redirect:/mmlWrite.do";
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR : MmlWriteAction - " + e.getMessage());
+		}
+		//작성자의 개인 mmlList로 이동하게 추후 링크조정
+		return "redirect:/mmlList.do";
 	}
 	@RequestMapping(value="/mmlFollowList.do", method=RequestMethod.GET)
 	public String mmlFollow() {
