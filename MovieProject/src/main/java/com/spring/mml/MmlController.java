a111909ea9da0da744c8da8b8412674fbf6ca511
AuroraPark <a20190212@gmail.com>
2019-07-30 21:00:16 +0900

    수정 : MERGE .. 웅식 / 유진 백단
    수정좀여,,

--- MovieProject/src/main/java/com/spring/mml/MmlController.java
+++ MovieProject/src/main/java/com/spring/mml/MmlController.java
@@ -1,13 +1,19 @@
 package com.spring.mml;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.mypage.MyPageService;
 
