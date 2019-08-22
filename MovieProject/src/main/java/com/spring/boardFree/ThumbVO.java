2644e1e150bdf49925a7bf70208227392c06b5fb
AuroraPark <a20190212@gmail.com>
2019-08-22 09:49:43 +0900

    boardfree 상필 추가

--- MovieProject/src/main/java/com/spring/boardFree/ThumbVO.java
+++ MovieProject/src/main/java/com/spring/boardFree/ThumbVO.java
@@ -10,5 +10,7 @@ public class ThumbVO { // 추천/비추천 공통 VO
 	private int bf_bno;
 	private int mr_code;
 	private int cr_code;
-	private int id;
+	private int id;	
+	private int mmlr_thumb;
+	private int mml_reply_code;
 }
